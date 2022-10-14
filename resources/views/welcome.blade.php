25679cccb2aadcfdcf9a96ee52aa18365ca89c92
zeeshan <zeeshantalking@gmail.com>
2022-10-14 16:51:32 +0000

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -117,7 +117,7 @@
                             </svg>
 
                             <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                                Sponsor
+                                Sponsor Zeeshan
                             </a>
                         </div>
                     </div>
